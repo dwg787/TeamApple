5a1aacd8cfea2ab9433f648e21a84f7a7a740125
dwg787 <forthevision7@gmail.com>
2023-01-10 18:53:34 +0900

    etc: pull 전 커밋 최신화

--- screens/Main.jsx
+++ screens/Main.jsx
@@ -1,58 +1,78 @@
 // import { useNavigation } from '@react-navigation/native';
 import { View, FlatList, SafeAreaView } from 'react-native';
-import { useQuery } from 'react-query';
-// import { getDetail } from '../api';
+import { useState } from 'react';
+import { useQuery, useQueryClient, useInfiniteQuery } from 'react-query';
 import styled from '@emotion/native';
-import iconSRC from '../assets/icon.png';
 import { fetchData } from '../api';
 import MainCard from '../components/MainCard';
+import Loader from '../components/Loader';
+// import { getDetail } from '../api';
+// import iconSRC from '../assets/icon.png';
 
 export default function Main() {
-  // const { navigate } = useNavigation();
+  const [isRefreshing, setIsRefreshing] = useState(false);
+  const queryClient = useQueryClient();
 
-  // const testList = [
-  //   {
-  //     card_kind: '토끼',
-  //     card_gender: '수컷',
-  //     card_age: '2022',
-  //     card_location: '서울특별시 마포구 마포동',
-  //     card_date: '2021-10-10',
-  //     card_picture: { iconSRC },
-  //   },
-  //   {
-  //     card_kind: '래브라도 리트리버',
-  //     card_gender: '암컷',
-  //     card_age: '2020',
-  //     card_location: '서울특별시 송파구 송파동',
-  //     card_date: '2021-1-13',
-  //     card_picture: { iconSRC },
-  //   },
-  // ];
+  // const {
+  //   data: rawData,
+  //   isLoading,
+  //   // isError,
+  //   // error,
+  // } = useQuery('animal_list', fetchData);
 
   const {
     data: rawData,
     isLoading,
-    // isError,
-    // error,
-  } = useQuery('animal_list', fetchData);
+    isFetching,
+    isFetchingNextPage,
+    isError,
+    error,
+    hasNextPage,
+    fetchNextPage,
+  } = useInfiniteQuery(
+    ['animal_list'],
+    ({ pageParam = 1 }) => fetchData(pageParam),
+    {
+      getNextPageParam: (lastPage) => {
+        if (lastPage.page < lastPage.total_pages) {
+          return lastPage.page + 1;
+        }
+      },
+    }
+  );
 
-  // const { data: rawData, isLoading } = useInfiniteQuery(
-  //   ['animal_list'],
-  //   fetchData
-  // );
+  const onRefresh = async () => {
+    setIsRefreshing(true);
+    // await getData();
+    //refetch
+    // await Promise.all();
+    await queryClient.refetchQueries(['animal_list']);
+    setIsRefreshing(false);
+  };
 
-  // const { isLoading, data, error } = useQuery(['animal'], getDetail);
+  const loadMoreData = async () => {
+    if (hasNextPage) {
+      await fetchNextPage();
+    }
+  };
 
-  // const loadMoreData = async () => {
-  //   if (hasNextData) {
-  //     await fetchNextPage();
-  //   }
-  // };
+  if (isError) {
+    console.log('error 내용:', error.message);
+    return (
+      <View>
+        <Text>오류가 발생하였습니다!</Text>
+      </View>
+    );
+  }
+
+  if (isLoading) {
+    return <Loader />;
+  }
 
   if (!isLoading) {
     const animalList = rawData?.data?.response?.body?.items?.item;
-    const totalPosting = animalList.length;
-    // console.log('LocationData:', animalList);
+    const totalPosting = animalList?.length || 0;
+    console.log('useInfiniteQuery 적용 api 호출', animalList);
     // const detailData = data.response.body.items.item;
     return (
       <SafeAreaView>
@@ -64,8 +84,11 @@ export default function Main() {
 
         <AnimalCardContainer>
           <FlatList
+            refreshing={isRefreshing}
+            onRefresh={onRefresh}
             showsHorizontalScrollIndicator={false}
-            // onEndReached={loadMoreData}
+            onEndReachedThreshold={0.5}
+            onEndReached={loadMoreData}
             data={animalList}
             renderItem={({ item }) => <MainCard item={item} />}
             keyExtractor={(item) => item.desertionNo}
@@ -97,71 +120,10 @@ const TextB = styled.Text`
   color: #ff0000;
 `;
 
-const TextC = styled.Text`
-  font-size: 15px;
-  font-weight: bold;
-  margin-top: 8px;
-`;
-
 const AnimalCardContainer = styled.View`
   width: 100%;
   height: 100%;
   flex-direction: column;
   background-color: #fff;
   padding: 20px;
-`;
-
-const SingleCard = styled.View`
-  flex-direction: row;
-  align-items: center;
-  width: 100%;
-  height: 170px;
-  margin-bottom: 15px;
-  border-radius: 10px;
-  background-color: #fff;
-  box-shadow: 2px 3px 2px;
-`;
-
-const AnimalCardPicture = styled.View`
-  margin-left: 25px;
-  width: 120px;
-  height: 120px;
-  border-radius: 70%;
-  background-color: black;
-`;
-
-const AnimalCardType = styled.View`
-  margin-left: 30px;
-  font-size: 25px;
-  font-weight: bold;
-`;
-
-const AnimalCardDescription = styled.View`
-  margin-left: 20px;
-  position: relative;
-`;
-
-const AnimalCardKind = styled.Text`
-  margin-top: 9px;
-  position: relative;
-`;
-
-const AnimalCardGender = styled.Text`
-  margin-top: 9px;
-  position: relative;
-`;
-
-const AnimalCardAge = styled.Text`
-  margin-top: 8px;
-  position: relative;
-`;
-
-const AnimalCardLocation = styled.Text`
-  margin-top: 8px;
-  position: relative;
-`;
-
-const AnimalCardDate = styled.Text`
-  margin-top: 6px;
-  position: relative;
 `;
